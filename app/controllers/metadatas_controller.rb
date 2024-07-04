78061d95c35c014ece28e2d33d18751ce3ccda34
HiroyasuTawara <tawara@netlab.jp>
2024-07-04 17:06:23 +0900

    2024-07-04 ソートロジックのメソッドへの切り出し

--- app/controllers/metadatas_controller.rb
+++ app/controllers/metadatas_controller.rb
@@ -2,14 +2,22 @@ class MetadatasController < ApplicationController
   def index
     @metadatas = Metadata.parse_xml
 
-    @metadatas.sort!{ |m1, m2|
-      if m1[:publication_date].blank?
-        1
-      elsif m2[:publication_date].blank?
-        -1
-      else
-        Date.parse(m2[:publication_date]) <=> Date.parse(m1[:publication_date])
+    @metadatas.sort!{|m1, m2| sort_logic(params[:sort_order], m1[params[:sort_column].to_sym], m2[params[:sort_column].to_sym])}
+  end
+  
+  private
+  
+  def sort_logic(ord, m1, m2)
+    if m1.blank?
+      1
+    elsif m2.blank?
+      -1
+    else
+      if ord.to_sym == :desc
+        Date.parse(m2) <=> Date.parse(m1)
+      elsif ord.to_sym == :asc
+        Date.parse(m1) <=> Date.parse(m2)
       end
-    }
+    end
   end
 end
